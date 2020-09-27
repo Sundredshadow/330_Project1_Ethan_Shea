2539ea42ccf2d52174a7997c4576f234cb2cd95b
Sundredshadow <54553082+Sundredshadow@users.noreply.github.com>
2020-09-26 20:15:24 -0400

    Add files via upload

--- src/edsLIB.js
+++ src/edsLIB.js
@@ -8,69 +8,8 @@ for(let z=0;z<1000;z++)
 }   
 
 
-window.onload = init;
-let width=640;//canvas height and width
-let height=480;
-let rows=height/5;
-let cols=width/5;
-let canvas;
-let ctx;
-let blockType=1;//current type of block on click placed
-let penSize=2;
-
-//a values aka speed values
-let gravitySpeed=1;//constant vel going downward
-let slideSpeed=1;//constant vel going downward
-let risingSpeed=1;//amount gas is allowed to rise upward per frame
-let flowSpeed=1;//current flow speed of water
-let flowChance=1
-
-let basetime=Date.now();
-let fpsCap=1000/60;//denominator is fps
-
-//intialize
-function init(){
-    
-    // #2 Now that the page has loaded, start drawing!
-    console.log('init called');
-    
-    // A - canvas variable points at <canvas> tag
-    canvas= document.querySelector("canvas");
-    canvas.height=height;
-    canvas.width=width;
-    // B - the ctx variable points at a "2D drawing context"
-    ctx=canvas.getContext("2d");
-    
-
-    //event listeners
-    canvas.addEventListener('mousedown',addBlocks,false);
-    canvas.addEventListener('click',mouseClick,false);
-    canvas.addEventListener('mouseleave',mouseClick,false);
-    canvas.addEventListener('mousemove',mouseMove,false);
-
-    cls(ctx);
-    setupUI();
-    update(ctx);
-}
-
-let play=true;
-//core drive functions aka update/draw
-function update(){
-    let now=Date.now();
-    let check=now-basetime;
-    if(check/fpsCap>=1){
-        basetime=now;
-        if(play){
-            cls(ctx); 
-            index.physics();
-            draw(ctx);
-        }
-    }
-    requestAnimationFrame(update);
-}
-function draw(ctx){
+function draw(ctx,cols,rows,blocks){
     //let a=drawFewer(ctx);
-    let blocks=index.GetBlocks();
     for(let i=0;i<cols;i++)
     {
         for(let z=0;z<rows;z++)
@@ -119,7 +58,7 @@ function draw(ctx){
     }	
 }
 
-function cls(ctx){//clear screen
+function cls(ctx,width,height){//clear screen
     ctx.clearRect(0,0,width,height);
     drawRectangle(ctx,0,0,width,height);
 }
@@ -151,141 +90,55 @@ function drawRectangle(ctx,x,y,width,height,fillStyle="black",lineWidth=0,stroke
     ctx.restore();
 }
 
+//other useful functions not used for this project////////////////////////////////////////////
+function drawRandomRect(ctx){
+    drawRectangle(ctx,getRandomInt(0,canvasWidth),getRandomInt(0,canvasHeight),getRandomInt(drawParams.minRectWidth,drawParams.maxRectWidth),getRandomInt(drawParams.minRectWidth,drawParams.maxRectWidth),edsLIB.getRandomColor(),edsLIB.getRandomInt(drawParams.minStrokeWidth,drawParams.maxStrokeWidth),edsLIB.getRandomColor());
+}
 
-//click and drag functionality
-let timer;//time id for click and hold
-let xClient=0;
-let yClient=0;    
-function addBlocks(e)
+function drawRandomCircle(ctx){
+    drawCircle(ctx,getRandomInt(0,canvasWidth),getRandomInt(0,canvasHeight),getRandomInt(20,300),getRandomColor(),getRandomInt(drawParams.minStrokeWidth,drawParams.maxStrokeWidth),edsLIB.getRandomColor()); 
+}
+
+function drawRandomLine(ctx){
+    drawLine(ctx,getRandomInt(0,canvasWidth),getRandomInt(0,canvasWidth),getRandomInt(0,canvasWidth),getRandomInt(0,canvasWidth),getRandomInt(drawParams.minStrokeWidth,drawParams.maxStrokeWidth),edsLIB.getRandomColor())
+}
+
+function drawCircle(ctx,x,y,radius, fillStyle="black",lineWidth=0,strokeStyle="black")
 {
-    let blocks=index.GetBlocks();
-    let rect=canvas.getBoundingClientRect();
-    let centerX=Math.round((xClient-rect.left)/5)*5;
-    let centerY=Math.round((yClient-rect.top)/5)*5;
-    for(let c=-1;c<penSize;c++)
+    ctx.fillStyle=fillStyle;
+    ctx.save();
+    ctx.beginPath();
+    ctx.arc(x,y,radius,0,Math.PI*2,false); 
+    ctx.closePath();
+    ctx.fill();
+    if(lineWidth>0)
     {
-        for(let a=-1;a<penSize;a++)
-        {
-            if(centerX+(c*5)<640&&centerX+(c*5)>=0){
-                blocks[((centerX+(c*5))/5)+(((centerY+(a*5))/5))*cols]=blockType;
-            }
-        }
+        ctx.lineWidth=lineWidth;
+        ctx.strokeStyle=strokeStyle;
+        ctx.stroke();
     }
-    //console.log(x+","+y);
-    timer=setTimeout(function(){addBlocks(e)},0);
+    ctx.restore();
 }
-function mouseMove(e){
-    xClient=e.clientX;
-    yClient=e.clientY;
-}
-function mouseClick(e)
+function drawLine(ctx,x1,y1,x2,y2,lineWidth=1,strokeStyle="black")
 {
-    clearTimeout(timer);
-}
-
-//setup button events
-function setupUI()
-{
-    //block section
-    document.querySelector("#Void").onclick=function()
+    ctx.save();
+    ctx.beginPath();
+    ctx.moveTo(x1,y1);
+    ctx.lineTo(x2,y2);
+    ctx.closePath(); 
+    if(lineWidth<=0)
     {
-        blockType=0;
-    };
-    document.querySelector("#Sand").onclick=function()
-    {
-        blockType=1;
-    };
-    document.querySelector("#Water").onclick=function()
-    {
-        blockType=2;
-    };
-    document.querySelector("#Salt").onclick=function()
-    {
-        blockType=3;
-    };
-    document.querySelector("#Smoke").onclick=function()
-    {
-        blockType=4;
-    };
-    document.querySelector("#Wall").onclick=function()
-    {
-        blockType=5;
-    };
-    document.querySelector("#SaltWater").onclick=function()
-    {
-        blockType=6;
-    };
-    document.querySelector("#Oil").onclick=function()
-    {
-        blockType=7;
-    };
-    document.querySelector("#Acid").onclick=function()
-    {
-        blockType=8;
-    };
-    document.querySelector("#Mercury").onclick=function()
-    {
-        blockType=9;
-    };
-    document.querySelector("#Fire").onclick=function()
-    {
-        blockType=10;
-    };
-    //slider section
-    document.querySelector("#gravitySpeed").oninput = function() {
-        gravitySpeed = this.value;
-    }		
-    document.querySelector("#flowChance").oninput = function() {
-        flowChance = this.value;
-    }	
-    document.querySelector("#flowSpeed").oninput = function() {
-        flowSpeed = this.value;
-    }	
-    document.querySelector("#penSize").oninput = function() {
-        penSize = this.value;
+        lineWidth=1;
     }
-    //other
-    document.querySelector("#Reset").onclick=function()
-    {
-        index.WipeBlocks();
-    };
-    document.querySelector("#PausePlay").onclick=function()
-    {
-        play=!play;
-    };
-}
-
-
-//speed getters//////////////////////////
-function GetGravity()
-{
-    return gravitySpeed;
-}
-
-function GetSlide()
-{
-    return slideSpeed;
-}
-
-function GetFlowSpeed()
-{
-    return flowSpeed;
-}
-
-function GetRising()
-{
-    return risingSpeed;
-}
-
-function GetFlowChance()
-{
-    return flowChance;
+    ctx.lineWidth=lineWidth;
+    ctx.strokeStyle=strokeStyle
+    ctx.stroke();
+    ctx.restore();
 }
 
 function GetRandomIntegerArr()
 {
     return randoms;
 }
-
-window.edsLIB={GetRandomIntegerArr,GetGravity,GetSlide,GetFlowSpeed,GetFlowChance,GetRising};
+window.edsLIB={getRandomInt,GetRandomIntegerArr,cls,draw,drawRandomRect,drawRandomCircle,drawRandomLine,drawLine,drawCircle};
 })()
--- src/index.js
+++ src/index.js
@@ -1,53 +1,199 @@
-// self.onmessage=function(e){
-//     console.log("loaded web worker");
-//     physics();
-//     this.postMessage(blocks);
-// }
 (function(){
-'use strict';
 
-let width=640;//canvas height and width
-let height=480;
-let rows=height/5;
-let cols=width/5;
+    window.onload = init;
+    let canvas;
+    let ctx;
+    let blockType=1;//current type of block on click placed
+    let penSize=2;
+    
+    //a values aka speed values
+    let gravitySpeed=1;//constant vel going downward
+    let slideSpeed=1;//constant vel going downward
+    let risingSpeed=1;//amount gas is allowed to rise upward per frame
+    let flowSpeed=1;//current flow speed of water
+    let flowChance=1
+    
+    let width=640;//canvas height and width
+    let height=480;
+    let rows=height/5;
+    let cols=width/5;
 
-//populates a 2 dimensional array
-let blocks= new Uint8Array(cols*rows).fill(0);//contains all blocks
-let movedAlready=new Uint8Array(cols*rows).fill(0);//skips over already moved through blocks
 
+    let basetime=Date.now();
+    let fpsCap=1000/60;//denominator is fps
+    
+    let play=true;
+//intialize
+function init(){
+    
+    // #2 Now that the page has loaded, start drawing!
+    console.log('init called');
+    
+    // A - canvas variable points at <canvas> tag
+    canvas= document.querySelector("canvas");
+    canvas.height=height;
+    canvas.width=width;
+    // B - the ctx variable points at a "2D drawing context"
+    ctx=canvas.getContext("2d");
+    
 
-//worker
-let worker;
-if(window.Worker){
-    worker=new Worker('src/physicsWorker.js');
-    worker.addEventListener("message",(d)=>{
-        blocks=d.data.physicsDataResult.blocks;
-        movedAlready=d.data.physicsDataResult.movedAlready;
-    });
+    //event listeners
+    canvas.addEventListener('mousedown',addBlocks,false);
+    canvas.addEventListener('click',mouseClick,false);
+    canvas.addEventListener('mouseleave',mouseClick,false);
+    canvas.addEventListener('mousemove',mouseMove,false);
+
+    edsLIB.cls(ctx,width,height);
+    setupUI();
+    update(ctx);
 }
-//getRandomInt,GetGravity,GetSlide,GetFlowSpeed,GetFlowChance,GetRising
-//physics functions//////////////////////////////////////////////////////////////////////////
-function physics(){
-    //worker code
-    let message={physicsData:{blocks:blocks,movedAlready:movedAlready,
-        gravitySpeed:edsLIB.GetGravity(),
-        slideSpeed:edsLIB.GetSlide(),
-        flowSpeed:edsLIB.GetFlowSpeed(),
-        flowChance:edsLIB.GetFlowChance(),
-        randomInts:edsLIB.GetRandomIntegerArr()}}
-    worker.postMessage(message);
+
+//core drive functions aka update/draw
+function update(){
+    let now=Date.now();
+    let check=now-basetime;
+    if(check/fpsCap>=1){
+        basetime=now;
+        if(play){
+            edsLIB.cls(ctx,width,height); 
+            physics.physics();
+            edsLIB.draw(ctx,cols,rows,physics.GetBlocks());
+        }
+    }
+    requestAnimationFrame(update);
 }
 
-function GetBlocks()
+//click and drag functionality
+let timer;//time id for click and hold
+let xClient=0;
+let yClient=0;    
+function addBlocks(e)
 {
-    return blocks;
+    if(play){
+    let blocks=physics.GetBlocks();
+    let rect=canvas.getBoundingClientRect();
+    let centerX=Math.round((xClient-rect.left)/5)*5;
+    let centerY=Math.round((yClient-rect.top)/5)*5;
+    for(let c=-1;c<penSize;c++)
+    {
+        for(let a=-1;a<penSize;a++)
+        {
+            if(centerX+(c*5)<640&&centerX+(c*5)>=0){
+                blocks[((centerX+(c*5))/5)+(((centerY+(a*5))/5))*cols]=blockType;
+            }
+        }
+    }
+    }
+    //console.log(x+","+y);
+    timer=setTimeout(function(){addBlocks(e)},0);
+}
+function mouseMove(e){
+    xClient=e.clientX;
+    yClient=e.clientY;
+}
+function mouseClick(e)
+{
+    clearTimeout(timer);
 }
 
-//functionality for reset with reload page
-function WipeBlocks(){
-    blocks= new Uint8Array(cols*rows).fill(0);//contains all blocks
-    movedAlready=new Uint8Array(cols*rows).fill(0);//skips over already moved through blocks
+//setup button events
+function setupUI()
+{
+    //block section
+    document.querySelector("#Void").onclick=function()
+    {
+        blockType=0;
+    };
+    document.querySelector("#Sand").onclick=function()
+    {
+        blockType=1;
+    };
+    document.querySelector("#Water").onclick=function()
+    {
+        blockType=2;
+    };
+    document.querySelector("#Salt").onclick=function()
+    {
+        blockType=3;
+    };
+    document.querySelector("#Smoke").onclick=function()
+    {
+        blockType=4;
+    };
+    document.querySelector("#Wall").onclick=function()
+    {
+        blockType=5;
+    };
+    document.querySelector("#SaltWater").onclick=function()
+    {
+        blockType=6;
+    };
+    document.querySelector("#Oil").onclick=function()
+    {
+        blockType=7;
+    };
+    document.querySelector("#Acid").onclick=function()
+    {
+        blockType=8;
+    };
+    document.querySelector("#Mercury").onclick=function()
+    {
+        blockType=9;
+    };
+    document.querySelector("#Fire").onclick=function()
+    {
+        blockType=10;
+    };
+    //slider section
+    document.querySelector("#gravitySpeed").oninput = function() {
+        gravitySpeed = this.value;
+    }		
+    document.querySelector("#flowChance").oninput = function() {
+        flowChance = this.value;
+    }	
+    document.querySelector("#flowSpeed").oninput = function() {
+        flowSpeed = this.value;
+    }	
+    document.querySelector("#penSize").oninput = function() {
+        penSize = this.value;
+    }
+    //other
+    document.querySelector("#Reset").onclick=function()
+    {
+        physics.WipeBlocks();
+        edsLIB.cls(ctx,width,height);
+    };
+    document.querySelector("#PausePlay").onclick=function()
+    {
+        play=!play;
+    };
 }
 
-window.index={physics,GetBlocks,WipeBlocks};
+
+//speed getters//////////////////////////
+function GetGravity()
+{
+    return gravitySpeed;
+}
+
+function GetSlide()
+{
+    return slideSpeed;
+}
+
+function GetFlowSpeed()
+{
+    return flowSpeed;
+}
+
+function GetRising()
+{
+    return risingSpeed;
+}
+
+function GetFlowChance()
+{
+    return flowChance;
+}
+window.index={GetGravity,GetSlide,GetFlowSpeed,GetFlowChance,GetRising};
 })()
